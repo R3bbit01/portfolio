f77ece407e2888600c2be51d98c9201c6e4a25a6
james <james@DESKTOP-DTKO2G9>
2023-10-22 15:50:00 -0500

    Add project files.

---
+++ PythonApplication6/PythonApplication6.py
@@ -0,0 +1,18 @@
+name = 'James'
+print("Hello ", name, "would you like to play a game")
+
+print()
+name = 'James'
+print("Hello ", name.upper(), "would you like to play a game")
+print("Hello ", name.lower(), "would you like to play a game")
+print("Hello ", name.title(), "would you like to play a game")
+
+
+name = 'James  '
+print("Hello ", name.rstrip(), "would you like to play a game")
+
+name = '  James  '
+print("Hello ", name.strip(), "would you like to play a game")
+
+name = '  James'
+print("Hello ", name.lstrip(), "would you like to play a game")
